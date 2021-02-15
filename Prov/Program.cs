1f2e826e16bdb8538f579a26d5335eab2cfab2f5
Ludvig-Andersson <Ludvig.andersson@elev.ga.ntig.se>
2021-02-15 09:37:18 +0100

    first commit

---
+++ Prov/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Prov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string fråga1 = "ja";
+
+
+
+
+            while (fråga1 == "ja")
+            {
+
+                System.Console.WriteLine("Hur mycket HP vill du ha, du kan endast välja mellan 1-20?");
+
+
+
+
+                string tal = Console.ReadLine();
+
+
+                int resultat;
+
+
+                while (resultat < 0 && resultat > 20)
+                {
+                    System.Console.WriteLine("Du skrev inte in ett nummer mellan 1-20");
+
+
+                    bool lyckad = int.TryParse(tal, out resultat);
+
+                    resultat = Console.ReadLine();
+                }
+
+
+
+                System.Console.WriteLine("HP " + tal);
+
+
+
+                Console.ReadLine();
+
+            }
+
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
